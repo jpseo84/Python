07785f861c60d201e2f0ebd117d321dce5d1738e
jpseo84 <jpseo84@gmail.com>
2021-02-20 19:15:26 +0900

    added lf/crlf

--- Test.py
+++ Test.py
@@ -1,8 +1,14 @@
 from itertools import (takewhile,repeat)
 
-def rawincount(filename):
+def rawincount_lf(filename):
     f = open(filename, 'rb')
     bufgen = takewhile(lambda x: x, (f.raw.read(1024*1024) for _ in repeat(None)))
     return sum( buf.count(b'\n') for buf in bufgen )
 
-print(rawincount("Test.csv"))
+def rawincount_crlf(filename):
+    f = open(filename, 'rb')
+    bufgen = takewhile(lambda x: x, (f.raw.read(1024*1024) for _ in repeat(None)))
+    return sum( buf.count(b'\r\n') for buf in bufgen )
+
+print('Total lf:', rawincount_lf("Test.csv"))
+print('Total crlf', rawincount_crlf("Test.csv"))
