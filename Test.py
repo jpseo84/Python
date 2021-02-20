f2537e82004fb7e4bf62ba773352482c954f7500
jpseo84 <jpseo84@gmail.com>
2021-02-20 15:37:57 +0900

    commit on 'main'

---
+++ Test.py
@@ -0,0 +1,8 @@
+from itertools import (takewhile,repeat)
+
+def rawincount(filename):
+    f = open(filename, 'rb')
+    bufgen = takewhile(lambda x: x, (f.raw.read(1024*1024) for _ in repeat(None)))
+    return sum( buf.count(b'\n') for buf in bufgen )
+
+print(rawincount("Test.csv"))
